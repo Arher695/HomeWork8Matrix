6fdd445f33e73d09758ceac81e60b29c5a64c4fe
Arher695 <sorokinivan695@gmail.com>
2025-08-18 21:36:25 +0700

    Add files via upload

--- Matrix.java
+++ Matrix.java
@@ -1,5 +1,3 @@
-package ru.netology;
-
 import java.util.Random;
 
 public class Matrix {
@@ -7,32 +5,37 @@ public class Matrix {
 
     public static void main(String[] args) {
         int[][] colors = new int[SIZE][SIZE];
-
-        System.out.println("Оригинал");
-        System.out.println();
-
         Random random = new Random();
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 colors[i][j] = random.nextInt(256);
             }
         }
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", colors[i][j]);
-            }
-            System.out.println();
-        }
+        System.out.println("Оригинал\n");
+        printArray(colors, SIZE);
         System.out.println();
-        System.out.println("На 90 градусов в право");
-        System.out.println();
-        int[][] rotatedColors90 = new int[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                rotatedColors90[i][j] = colors[SIZE - 1 - j][i];
-                System.out.format("%4d", rotatedColors90[i][j]);
+        System.out.println("На 90 градусов в право\n");
+        int[][] rotationArray90 = rotationArray90(colors, SIZE);
+        printArray(rotationArray90, SIZE);
+    }
+
+    public static void printArray(int[][] array, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.format("%4d", array[i][j]);
             }
             System.out.println();
         }
     }
+
+    public static int[][] rotationArray90(int[][] arrayInput, int size) {
+        int[][] rotationArray90 = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                rotationArray90[i][j] = arrayInput[size - 1 - j][i];
+            }
+        }
+        return rotationArray90;
+    }
 }
+
